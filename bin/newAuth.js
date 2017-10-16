42a401e96b0a49950015529745317744808756ce
SustainRelease <sirrobbiemuir@hotmail.com>
2017-10-16 12:29:27 -0500

    Fixed new auth file pathing

--- bin/newAuth.js
+++ bin/newAuth.js
@@ -1,7 +1,7 @@
 
 var authHelper = require("../node/authHelper.js");
 var path = require('path');
-var secretPath = path.join(__dirname,"data","client_secret.json");
+var secretPath = path.join(__dirname,"..", "data","client_secret.json");
 authHelper.getNewAuth(secretPath).then(function(auth) {
   console.log("Google authorization setup");
 }, function(reason) {
